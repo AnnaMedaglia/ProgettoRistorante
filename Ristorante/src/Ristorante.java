2196700f8cb865eeb7efee953ffbdf788f72d49e
Fernanda <Fernanda@192.168.1.27>
2023-05-04 23:51:56 +0200

    costruttore Ristorante

--- Ristorante/src/Ristorante.java
+++ Ristorante/src/Ristorante.java
@@ -1,4 +1,4 @@
-import java.util.Set;
+import java.util.HashSet;
 
 public class Ristorante {
 	
@@ -6,7 +6,17 @@ public class Ristorante {
 	private int caricoLavoroPersona;
 	private int numPosti;
 	private double caricoLavoroRistorante; 
-	private Set<GenereExtra> insiemeGE;
-	private Set<Bevanda> insiemeB;
-
+	private HashSet<GenereExtra> insiemeGE;
+	private HashSet<Bevanda> insiemeB;
+	
+	//costruttore
+	public Ristorante(String nome, int caricoLavoroPersona, int numPosti, double caricoLavoroRistorante) {
+		this.nome = nome;
+		this.caricoLavoroPersona = caricoLavoroPersona;
+		this.numPosti = numPosti;
+		this.caricoLavoroRistorante = caricoLavoroRistorante;
+		this.insiemeGE = new HashSet<>();
+		this.insiemeB = new HashSet<>();
+	}
+	
 }
