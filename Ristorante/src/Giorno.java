078922b0a21b19e3b5e79012570963c9e5fc66f5
Fernanda <Fernanda@192.168.1.27>
2023-05-04 21:52:43 +0200

    merce e giorno

--- Ristorante/src/Giorno.java
+++ Ristorante/src/Giorno.java
@@ -1,11 +1,12 @@
 import java.time.LocalDate;
 
 public class Giorno extends Periodo {
-	
-	
+		
 	private LocalDate giorno;
 	
 	public Giorno(LocalDate giorno) {
 		super (giorno, giorno);
 	}
+	
+	//get e setter
 }
---
+++ Ristorante/src/Merce.java
@@ -0,0 +1,10 @@
+
+public abstract class Merce {
+	
+	private String nome;
+	private String tipo;
+	private String unitàMisura;
+	private Giorno scadenza;
+	private boolean qualità;
+		
+}
